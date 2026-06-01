c1f18f3556c2dba1bd1aca13a9783367a3dfdd93
Hello Foodie <foodie@example.com>
2026-06-01 12:57:31 -0600

    style: uniformar logotipos al isotipo de gota/pin calado correcto

--- src/components/BrandLogo.jsx
+++ src/components/BrandLogo.jsx
@@ -5,59 +5,47 @@ export default function BrandLogo({ size = 64, className = "", animate = true })
         <svg
             width={size}
             height={size}
-            viewBox="0 0 100 120"
+            viewBox="0 0 512 512"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
             className={className}
         >
             <defs>
-                {/* Main Orange Gradient */}
-                <linearGradient id="logoGradient" x1="50" y1="5" x2="50" y2="115" gradientUnits="userSpaceOnUse">
-                    <stop offset="0%" stopColor="#FB923C" /> {/* Orange 400 */}
-                    <stop offset="100%" stopColor="#F43F5E" /> {/* Rose 500 - matches the vibrant orange/red look */}
+                {/* Master Gradient matching logo-app-mobile-icon.svg */}
+                <linearGradient id="brandLogoGradient" x1="20%" y1="0%" x2="80%" y2="100%">
+                    <stop offset="0%" stopColor="#FFB547" />
+                    <stop offset="40%" stopColor="#FB923C" />
+                    <stop offset="100%" stopColor="#E11D48" />
                 </linearGradient>
 
-                {/* Shadow for depth */}
-                <filter id="logoShadow" x="-10%" y="-10%" width="120%" height="120%">
-                    <feGaussianBlur in="SourceAlpha" stdDeviation="2" />
-                    <feOffset dx="0" dy="3" result="offsetblur" />
-                    <feComponentTransfer>
-                        <feFuncA type="linear" slope="0.15" />
-                    </feComponentTransfer>
-                    <feMerge>
-                        <feMergeNode />
-                        <feMergeNode in="SourceGraphic" />
-                    </feMerge>
+                {/* Fork and Spoon negative space mask */}
+                <mask id="brandLogoMask">
+                    <rect width="512" height="512" fill="white" />
+                    {/* Fork Cutout */}
+                    <path
+                        d="M 129 120 Q 129 110 136 110 Q 143 110 143 120 L 143 180 L 149 180 L 149 120 Q 149 110 156 110 Q 163 110 163 120 L 163 180 L 169 180 L 169 120 Q 169 110 176 110 Q 183 110 183 120 L 183 180 L 189 180 L 189 120 Q 189 110 196 110 Q 203 110 203 120 L 203 200 C 203 230 176 250 176 260 L 176 500 L 156 500 L 156 260 C 156 250 129 230 129 200 Z"
+                        fill="black"
+                    />
+                    {/* Spoon Cutout */}
+                    <path
+                        d="M 346 110 C 316 110 300 135 300 170 C 300 210 330 240 336 250 L 336 500 L 356 500 L 356 250 C 362 240 392 210 392 170 C 392 135 376 110 346 110 Z"
+                        fill="black"
+                    />
+                </mask>
+
+                {/* Soft modern glowing drop shadow */}
+                <filter id="brandLogoShadow" x="-20%" y="-20%" width="140%" height="140%">
+                    <feDropShadow dx="0" dy="12" stdDeviation="15" floodColor="#E11D48" floodOpacity="0.25" />
                 </filter>
             </defs>
 
-            {/* Main Pin Shape with Gradient */}
+            {/* Main Pin Shape with Gradient, Mask, and Shadow */}
             <path
-                d="M50 5C27.9 5 10 22.9 10 45C10 75 50 115 50 115C50 115 90 75 90 45C90 22.9 72.1 5 50 5Z"
-                fill="url(#logoGradient)"
-                filter="url(#logoShadow)"
+                d="M 256 460 C 380 340 436 280 436 200 A 180 180 0 0 0 76 200 C 76 280 132 340 256 460 Z"
+                fill="url(#brandLogoGradient)"
+                mask="url(#brandLogoMask)"
+                filter="url(#brandLogoShadow)"
             />
-
-            {/* The "S" Curve Highlight - This creates the depth seen in Opt 1 */}
-            <path
-                d="M50 5C68 5 85 20 85 45C85 68 50 105 50 105C50 105 52 75 52 45C52 25 50 5 50 5Z"
-                fill="white"
-                fillOpacity="0.12"
-            />
-
-            {/* Fork Cutout (Left Side) */}
-            <g transform="translate(28, 32) scale(0.9)">
-                {/* Fork Base */}
-                <path d="M12 25V42H16V25C20 25 22 22 22 18V5H19V18H17V5H14V18H12V5H9V18H7V5H4V18C4 22 6 25 10 25H12Z" fill="white" />
-            </g>
-
-            {/* Spoon Cutout (Right Side) */}
-            <g transform="translate(56, 32) scale(0.9)">
-                {/* Spoon Head */}
-                <ellipse cx="8" cy="14" rx="8" ry="11" fill="white" />
-                {/* Spoon Handle */}
-                <rect x="6" y="24" width="4" height="18" rx="2" fill="white" />
-            </g>
         </svg>
     );
 
